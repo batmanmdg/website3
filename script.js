5b8635cfda14e356c2f469b457a153362600ac1f
batmanmdg <patrick.r.poirier@gmail.com>
2025-06-05 16:29:18 -0400

    add js and split html

---
+++ script.js
@@ -0,0 +1,31 @@
+function launchFancyFireworks() {
+    const duration = 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 1000 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+        if (timeLeft <= 0) return clearInterval(interval);
+
+        const particleCount = 50 * (timeLeft / duration);
+        confetti(Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+            colors: ['#bb0000', '#ffffff', '#0000ff', '#00ff00', '#ffcc00']
+        }));
+        confetti(Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+            colors: ['#bb0000', '#ffffff', '#0000ff', '#00ff00', '#ffcc00']
+        }));
+    }, 250);
+}
+
+window.onload = () => {
+    launchFancyFireworks();
+    setInterval(launchFancyFireworks, 5000);
+};
